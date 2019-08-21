e0a46aec83d5f65d3b5daddc92a4059382400f2c
Ronisha White <ronishawhite@gmail.com>
2019-08-20 20:47:49 -0400

    clean

---
+++ src/Vt.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+
+import './App.css';
+
+function Vt() {
+  return (
+    
+    <div class="wrapper">
+    <div className="title-wrapper">
+      <h1><span className="bigger">35mm</span> <span className="box-border">Live</span> <span className="line-around">Shot</span> by <span className="big">Ronisha White</span></h1>
+    </div>
+
+  
+    <div className="imageContainer">
+    <div className="image">
+      <img src="images/vt1.jpg" height="500px" style={{maxWidth: '100%'}} alt="" />
+    </div>
+    <div className="image">
+      <img src="images/vt2.jpg" height="500px" style={{maxWidth: '100%'}} alt="" />
+    </div>
+    <div className="image">
+      <img src="images/vt3.jpg" height="500px" style={{maxWidth: '100%'}} alt="" />
+    </div>    
+    </div>
+  </div>
+   );
+  }
+
+
+
+export default Vt;
+
+
