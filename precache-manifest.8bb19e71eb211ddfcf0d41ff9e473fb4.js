a31beffda3f118679c13e68d1bdacc647380eea3
35mmlive <ronishaaa@gmail.com>
2019-08-20 22:12:25 -0400

    Updates

---
+++ precache-manifest.8bb19e71eb211ddfcf0d41ff9e473fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c4d12affc5264ad498fb60a55353d1",
+    "url": "/35mmlive/index.html"
+  },
+  {
+    "revision": "8db6efd9ecb09d93e25f",
+    "url": "/35mmlive/static/css/main.44279ff1.chunk.css"
+  },
+  {
+    "revision": "b4ca1b09a64682567178",
+    "url": "/35mmlive/static/js/2.d86e522c.chunk.js"
+  },
+  {
+    "revision": "8db6efd9ecb09d93e25f",
+    "url": "/35mmlive/static/js/main.efa54ff3.chunk.js"
+  },
+  {
+    "revision": "fb5753ae895da2fc1f3e",
+    "url": "/35mmlive/static/js/runtime~main.0f950f86.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Home/precache-manifest.7caedbf29459b33671e4e5eb2eae18bd.js"
+  "/35mmlive/precache-manifest.8bb19e71eb211ddfcf0d41ff9e473fb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/35mmlive/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
